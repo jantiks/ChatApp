9f8deaa41e440a061c24089bd7f858c3c386a554
Tigran Arsenyan <tigranforjob00@gmail.com>
2021-03-05 21:16:00 +0400

    sign in view done

---
+++ RealtimeChat/RealtimeChat/Models/Utils/UIColor_Swift.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor_Hex_Swift.swift
+//  RealtimeChat
+//
+//  Created by Tigran on 3/5/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var appDefault: UIColor {
+        return UIColor.systemBlue
+    }
+    
+    static var appGrey: UIColor {
+        return UIColor.systemGray3
+    }
+}
---
+++ RealtimeChat/RealtimeChat/Models/Utils/UtilsGeneral.swift
@@ -0,0 +1,12 @@
+//
+//  UtilsGeneral.swift
+//  RealtimeChat
+//
+//  Created by Tigran on 3/5/21.
+//
+
+import UIKit
+
+class UtilsGeneral: NSObject {
+    public static let defaultColor = UIColor.appDefault
+}
